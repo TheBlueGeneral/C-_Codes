c77f8cdde43faf827dd23664d79d346a42580539
NoobMaster69 <l126shashankra@gmail.com>
2024-10-28 23:49:45 +0530

    Create Obligatory_HelloWorld.cpp

---
+++ Obligatory_HelloWorld.cpp
@@ -0,0 +1,13 @@
+//  All codes are written using C++20 compiler.
+
+//In terminal, use the command below.
+//g++ -std=c++20 -o <executable_name> <file_name>.cpp
+//./<executable_name>
+
+#include <iostream>  
+
+int main()
+{
+  std::cout<<"Hello World!"<<std::endl;
+  return 0;
+}
